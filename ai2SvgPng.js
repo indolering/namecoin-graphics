271b3056ad211cf51fa3a6f703c56a2ae84a6a9f
indolering <zachlym@indolering.com>
2014-09-11 19:15:00 -0700

    Misc Final Touches on graphics

--- ai2SvgPng.js
+++ ai2SvgPng.js
@@ -6,50 +6,59 @@
 app.userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
 
 var projectFolder = Folder.selectDialog('Select project folder.');
-var aiFolder = new Folder ( projectFolder.absoluteURI + "/ai/");
-var files = aiFolder.getFiles( "*.ai" );
+var aiFolder = new Folder(projectFolder.absoluteURI + "/ai/");
+var files = aiFolder.getFiles("*.ai");
 
 
-if ( files.length == 0) {
+if(files.length == 0){
 
-  alert( 'No .ai files found in ' + aiFolder.absoluteURI + ", see README for directory layout requirements.");
+  alert('No .ai files found in ' + aiFolder.absoluteURI + ", see README for directory layout requirements.");
 
 } else {
 
-  for ( i = 0; i < files.length; i++ )
-  {
+  for(i = 0; i < files.length; i++){
     var doc = app.open(files[i]); // returns the document object
 
-    // SVG
-    var svgOpt = new ExportOptionsSVG();
-    svgOpt.embedRasterImages = true;
-    svgOpt.coordinatePrecision = 7;
+    //icons/favicons are handcrafted
+    if((doc.name.match(/.*icon.*/i) === null) && (doc.name.match(/.*circle.*/i) === null )){
 
-    doc.exportFile(
-      new File(projectFolder + '/svg/' + doc.name),
-      ExportType.SVG,
-      svgOpt);
-
-
-    // PNG
-    var sizes = [1000, 500, 200, 100]; //These folders must exist within the png directory!
-    var dimensions = doc.artboards[0].artboardRect.slice(1,2);
-    var longest = dimensions[0];
-    if(dimensions[0] < dimensions[1]){longest = dimensions[1]}
-
-    for(var j = 0; j < sizes.length; j++){
-      var size = sizes[j];
-      var scale = size/longest * 100;
-
-      var pngOpt = new ExportOptionsPNG24();
-      pngOpt.horizontalScale = pngOpt.verticalScale = scale;
-      pngOpt.artBoardClipping = true;
+      // SVG
+      var svgOpt = new ExportOptionsSVG();
+      svgOpt.embedRasterImages = true;
+      svgOpt.coordinatePrecision = 7;
 
       doc.exportFile(
-        new File(projectFolder + '/png/' + size + "/" + size + "-" + doc.name),
-        ExportType.PNG24,
-        pngOpt);
+        new File(projectFolder + '/svg/' + doc.name),
+        ExportType.SVG,
+        svgOpt);
+
+      //raster files don't need outlined
+      if(doc.name.match(/.*outlined.*/i) === null){
+
+        // PNG
+        var sizes = [1000, 500, 200, 100]; //These folders must exist within the png directory!
+        var dimensions = doc.artboards[0].artboardRect;
+        var longest = doc.width;
+        if(doc.width < doc.height){
+          longest = doc.height
+        }
+
+        for(var j = 0; j < sizes.length; j++){
+          var size = sizes[j];
+          var scale = size / longest * 100;
+
+          var pngOpt = new ExportOptionsPNG24();
+          pngOpt.horizontalScale = pngOpt.verticalScale = scale;
+          pngOpt.artBoardClipping = true;
+
+          doc.exportFile(
+            new File(projectFolder + '/png/' + size + "/" + size + "-" + doc.name),
+            ExportType.PNG24,
+            pngOpt);
+        }
+      }
     }
     doc.close(SaveOptions.DONOTSAVECHANGES);
+
   }
 }
